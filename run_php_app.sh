47c610cf97a9374c7ba1181088a0daf8e09bab82
mlamamra <mlamamra@dawan.fr>
2022-11-15 15:58:24 +0100

    bind mount pour injecter les confs dans les conteneurs

--- run_php_app.sh
+++ run_php_app.sh
@@ -36,7 +36,8 @@ docker run --name $server_container \
     -d --restart unless-stopped \
     -p 8080:80 \
     --net=app_net \
+    -v /vagrant/confs/nginx/app_php.conf:/etc/nginx/conf.d/app_php.conf:ro \
     nginx:1.22
 
-docker cp /vagrant/confs/nginx/app_php.conf $server_container:/etc/nginx/conf.d/app_php.conf
-docker restart $server_container
+# docker cp /vagrant/confs/nginx/app_php.conf $server_container:/etc/nginx/conf.d/app_php.conf
+# docker restart $server_container
