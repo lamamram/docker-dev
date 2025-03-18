948d34c4c1cda10a6d33a66ddd919a9f822b1df2
mlamamra <mlamamra@dawan.fr>
2025-03-18 12:23:48 +0100

    montage NFS

--- stack-php/vol-nfs.sh
+++ stack-php/vol-nfs.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
+## pour réexécuter le script : sudo rm -rf /mnt/nfs-dir /etc/exports
+# puis ./vol-nfs.sh
+if [ -d "/mnt/nfs-dir" ]; then
+  sudo rm -rf /mnt/nfs-dir && sudo rm -rf /etc/exports
+fi
 
-# # ajout des éléments à utiliser à distance
-# sudo apt-get install -y nfs-kernel-server
-# sudo mkdir /mnt/nfs-dir
-# cp index.php php8.2.conf mariadb-init.sql /mnt/nfs-dir
+# conf "vanilla" pas prod !!!
+sudo apt-get update
+sudo apt-get install -y nfs-kernel-server
+sudo mkdir -p /mnt/nfs-dir/{nginx-conf.d,app,initdb.d}
+sudo cp ./vhost.conf /mnt/nfs-dir/nginx-conf.d
+sudo cp ./index.php /mnt/nfs-dir/app
+sudo cp ./mariadb-init.sql /mnt/nfs-dir/initdb.d
+sudo chown -R nobody:nogroup /mnt/nfs-dir
+find /mnt/nfs-dir -type d -print0 | sudo xargs -0 chmod 0755
+find /mnt/nfs-dir -type f -print0 | sudo xargs -0 chmod 0644
+echo "/mnt/nfs-dir *(rw,sync,no_subtree_check,no_all_squash)" | sudo tee -a /etc/exports
+sudo exportfs -a
+sudo systemctl restart nfs-kernel-server
 
-# # conf nfs "vanilla"
-# sudo chown -R nobody:nogroup /mnt/nfs-dir
-# sudo chmod 755 /mnt/nfs-dir
-# sudo chmod 644 /mnt/nfs-dir/*
-# # configuration server nfs
-# echo "/mnt/nfs-dir *(rw,sync,no_subtree_check,no_all_squash)" | sudo tee -a /etc/exports
-# sudo exportfs -a
-# sudo systemctl restart nfs-kernel-server
-
-docker volume create \
-       --driver local \
-       --opt type=nfs \
-       --opt o=addr=192.168.1.30,ro \
-       --opt device=:/mnt/nfs-dir \
-       nfs-vol
+# docker volume create \
+#        --driver local \
+#        --opt type=nfs \
+#        --opt o=addr=192.168.1.30,ro \
+#        --opt device=:/mnt/nfs-dir \
+#        nfs-vol
 
 
